f8d0da6304581b915b8e441fdd6777339969f15f
bredele <olivier.wietrich@gmail.com>
2017-12-31 09:30:08 -0700

    Accept multiple endpoints

--- test/manner-to-schema.js
+++ test/manner-to-schema.js
@@ -6,13 +6,37 @@ const test = require('tape')
 const schema = require('..')
 
 
-test('should generate default schema from basic endpoint', assert => {
+test('should generate default schema from single and simple endpoint', assert => {
   assert.plan(1)
   const obj = schema({
     get() {}
   })
   assert.deepEqual(obj, {
     get: {
+      '/': {
+        query: {},
+        body: {}
+      }
+    }
+  })
+
+})
+
+
+test('should generate default schema from multiple simple endpoints', assert => {
+  assert.plan(1)
+  const obj = schema({
+    get() {},
+    post() {}
+  })
+  assert.deepEqual(obj, {
+    get: {
+      '/': {
+        query: {},
+        body: {}
+      }
+    },
+    post: {
       '/': {
         query: {},
         body: {}
